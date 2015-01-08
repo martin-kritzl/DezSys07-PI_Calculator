b21045b92189634c00cd03f722b2482270cf069c
serceg-tgm <serceg@student.tgm.ac.at>
2015-01-08 21:16:50 +0100

    began to implement BalancerAlgorithm

--- src/at/erceg_kritzl/pi_calculator/algorithm/SequenceAlgorithm.java
+++ src/at/erceg_kritzl/pi_calculator/algorithm/SequenceAlgorithm.java
@@ -1,5 +1,8 @@
 package at.erceg_kritzl.pi_calculator.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import at.erceg_kritzl.pi_calculator.service.Service;
 
 public class SequenceAlgorithm implements BalancerAlgorithm {
@@ -7,23 +10,25 @@ public class SequenceAlgorithm implements BalancerAlgorithm {
 	private Service service;
 
 	public SequenceAlgorithm(Service service) {
-		
-		this.service = service;
-	
-	}
 
+		this.service = service;
+
+	}
 
 	/**
 	 * @see at.erceg_kritzl.pi_calculator.algorithm.BalancerAlgorithm#getServerName()
 	 * 
-	 *  
+	 * 
 	 */
 	public String getServerName() {
-		
+
+		List<String> availableServers = new ArrayList<String>();
+
 		if (this.service.getServerNames().size() > 0) {
-			for(int i = 0; i < this.service.getServerNames().size(); i++) {
-				return this.service.getServerNames().get(i);
-			} 
+			for (int i = 0; i < this.service.getServerNames().size(); i++) {
+				availableServers.addAll(this.service.getServerNames());
+				availableServers.remove();
+			}
 		} else {
 			System.out.println("Kein Server momentan vorhanden");
 		}
